704ca2982933141df705ad2e451fad9458d2d54c
rhey <rheychristian26@gmail.com>
2026-03-01 15:39:56 +0800

    ok na

--- restaurant_site/settings.py
+++ restaurant_site/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 # STATIC & MEDIA FILES
 # -------------------------------------------------------------------
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'restaurant_site' / 'static']
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
